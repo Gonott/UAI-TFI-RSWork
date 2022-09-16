27065f86c71b1877d6f680a307eb7453aaeb1286
Otto <gonott@gmail.com>
2022-09-16 19:24:43 -0300

    Creacion de pagina de Parametros del Alquiler

---
+++ RSWork/ParametrosDelAlquiler.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Title="ParametrosDelAlquiler" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ParametrosDelAlquiler.aspx.cs" Inherits="RSWork.ParametrosDelAlquiler" %>
+
+
+<asp:Content ID="ParametrosDelAlquiler" ContentPlaceHolderID="MainContent" runat="server" method="post">
+    
+    <article>
+        <div>
+            <p>Seleccione los Empleados para los que desea los elementos.</p>
+            <asp:ListBox runat="server"></asp:ListBox>
+        </div>
+
+        <div>
+            <p>Fecha de inicio del alquiler.</p>
+            <asp:Calendar class="datepicker" ID="CalendarioInicio" runat="server"></asp:Calendar>
+            <p>Tiempo de Alquiler: <asp:TextBox runat="server"/> 
+                <asp:RadioButtonList ID="RadioButtonList1" runat="server">
+                    <asp:ListItem Text="Días" />
+                    <asp:ListItem Text="Semanas" />
+                    <asp:ListItem Text="Meses"/>
+                    <asp:ListItem Text="Años" />
+                 </asp:RadioButtonList>
+            </p>
+        </div>
+        <asp:Button Text="Continuar" ID="BtnContinuar" runat="server" />
+
+    </article>
+
+
+
+
+
+</asp:Content>
---
+++ RSWork/ParametrosDelAlquiler.aspx.designer.cs
@@ -0,0 +1,44 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace RSWork
+{
+
+
+    public partial class ParametrosDelAlquiler
+    {
+
+        /// <summary>
+        /// Control CalendarioInicio.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Calendar CalendarioInicio;
+
+        /// <summary>
+        /// Control RadioButtonList1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButtonList RadioButtonList1;
+
+        /// <summary>
+        /// Control BtnContinuar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnContinuar;
+    }
+}

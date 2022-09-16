b4f70c899f353539216d234dbca4557861faae5e
Otto <gonott@gmail.com>
2022-09-16 01:09:55 -0300

    Creacion del Pagina de Perfil Cliente.

---
+++ RSWork/PerfilCliente.aspx
@@ -0,0 +1,58 @@
+﻿<%@ Page Title="Perfil de Cliente" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="PerfilCLiente.aspx.cs" Inherits="RSWork.PerfilCliente" %>
+
+
+<asp:Content ID="PerfilProveedor" ContentPlaceHolderID="MainContent" runat="server" method="post">
+    
+
+    <article>
+        <h2> Datos de Empresa</h2>
+        <label ID="LabelCUIT" runat="server" Text="CUIT"></Label>
+        <label ID="LabelNombreEmp" runat="server" Text="Nombre Empresa S.A."></Label>
+        <label ID="LabelCat" runat="server" Text="Categoria"></Label>
+        <label ID="LabelCod" runat="server" Text="Codigo1234"></Label>
+        <br />
+        <label> Teléfono </label> <input type="text" name="TxtBoxTelefono" value="Telefono" />
+        <label> Correo Electrónico</label> <input type="text" name="TxtBoxEmail" value="Correo" /> 
+        <br />
+        <label> Dirección </label> <input type="text" name="TextBoxDireccion" value="" />
+        <asp:Button ID="ModificarBtn" runat="server" Text="Button" />
+    </article>
+
+    <article>
+        <div><h2>Mis Empleados</h2>
+            <asp:ListBox ID="ListBox1" runat="server">
+            </asp:ListBox>  
+
+        </div>
+        <div>
+            <p>DNI:<asp:TextBox ID="TextBoxDNI" runat="server"></asp:TextBox> </p>
+            <p>Nombre: <asp:TextBox ID="TextBoxNombreEmp" runat="server"></asp:TextBox></p>
+            <p>Dirección:<asp:TextBox ID="TextBoxDireccionEmp" runat="server"></asp:TextBox> </p>
+            <p>Correo Electrónico <asp:TextBox ID="TextBoxEmail" runat="server"></asp:TextBox></p>
+        </div>
+        
+        <asp:Button ID="BtnCrear" runat="server" Text="Crear" />
+        <asp:Button ID="BtnModificar" runat="server" Text="Modificar" />
+        <asp:Button ID="BtnEliminar" runat="server" Text="Eliminar" />
+
+    </article>
+
+
+    <article>
+        <h2>Mis Contratos</h2>
+        <div>
+            <label>Mis Contratos Activos</label><br />
+            <asp:ListBox ID="ListBoxContratos" runat="server">
+
+            </asp:ListBox>                                             
+        </div>
+        <div>
+            <label>Contrato</label>
+
+        </div>
+
+
+    </article>
+
+
+</asp:Content>
---
+++ RSWork/PerfilCliente.aspx.designer.cs
@@ -0,0 +1,143 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace RSWork
+{
+
+
+    public partial class PerfilCliente
+    {
+
+        /// <summary>
+        /// Control LabelCUIT.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl LabelCUIT;
+
+        /// <summary>
+        /// Control LabelNombreEmp.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl LabelNombreEmp;
+
+        /// <summary>
+        /// Control LabelCat.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl LabelCat;
+
+        /// <summary>
+        /// Control LabelCod.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl LabelCod;
+
+        /// <summary>
+        /// Control ModificarBtn.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button ModificarBtn;
+
+        /// <summary>
+        /// Control ListBox1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ListBox ListBox1;
+
+        /// <summary>
+        /// Control TextBoxDNI.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBoxDNI;
+
+        /// <summary>
+        /// Control TextBoxNombreEmp.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBoxNombreEmp;
+
+        /// <summary>
+        /// Control TextBoxDireccionEmp.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBoxDireccionEmp;
+
+        /// <summary>
+        /// Control TextBoxEmail.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBoxEmail;
+
+        /// <summary>
+        /// Control BtnCrear.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnCrear;
+
+        /// <summary>
+        /// Control BtnModificar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnModificar;
+
+        /// <summary>
+        /// Control BtnEliminar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnEliminar;
+
+        /// <summary>
+        /// Control ListBoxContratos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ListBox ListBoxContratos;
+    }
+}

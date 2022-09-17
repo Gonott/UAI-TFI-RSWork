e899c849d7c81f34efd3254de514b0d325921f67
Otto <gonott@gmail.com>
2022-09-16 21:15:58 -0300

    Escritura en ABM Publicaciones Page.

--- RSWork/ABMPublicaciones.aspx
+++ RSWork/ABMPublicaciones.aspx
@@ -5,8 +5,31 @@
 <asp:Content ID="ABMPublicaciones" ContentPlaceHolderID="MainContent" runat="server" method="post">
 
     <article>
-        <h2>Mis Publicaciones</h2>
+        <h2>Publicación</h2>
+        <p>Nombre de la Publicación  <asp:TextBox ID="TextBoxNombrePub" runat="server">Nombre</asp:TextBox>  </p>
+
+        <div>
+            <p>Seleccione un Elemento para la Publicación</p>
+            <asp:ListBox runat="server">
+                <asp:ListItem Text="text1" />
+                <asp:ListItem Text="text2" />
+            </asp:ListBox>
+            <p>Cantidad Publicada: <asp:TextBox ID="TextBoxCantidad" runat="server">Cantidad</asp:TextBox></p>
+        </div>
+
+        <div>
+            <div>
+                <asp:Image ImageUrl="imageurl" runat="server" />
+                <br />
+                <asp:TextBox ID="TextBoxResumen" runat="server">Agregue un Resumen para mostrar</asp:TextBox>
+            </div>
+            <asp:FileUpload ID="FileUploadImgPub" runat="server" />
+        </div>
         
+        <asp:Button ID="ButtonPublicar" runat="server" Text="Publicar" />
+        <asp:Button Text="Modificar" runat="server" ID ="ButtonModificar" />
+        <asp:Button Text="Eliminar" runat="server" ID="ButtonEliminar" />
+
     </article>
 
 
--- RSWork/ABMPublicaciones.aspx.designer.cs
+++ RSWork/ABMPublicaciones.aspx.designer.cs
@@ -13,5 +13,68 @@ namespace RSWork
 
     public partial class ABMPublicaciones
     {
+
+        /// <summary>
+        /// Control TextBoxNombrePub.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBoxNombrePub;
+
+        /// <summary>
+        /// Control TextBoxCantidad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBoxCantidad;
+
+        /// <summary>
+        /// Control TextBoxResumen.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBoxResumen;
+
+        /// <summary>
+        /// Control FileUploadImgPub.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.FileUpload FileUploadImgPub;
+
+        /// <summary>
+        /// Control ButtonPublicar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button ButtonPublicar;
+
+        /// <summary>
+        /// Control ButtonModificar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button ButtonModificar;
+
+        /// <summary>
+        /// Control ButtonEliminar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button ButtonEliminar;
     }
 }

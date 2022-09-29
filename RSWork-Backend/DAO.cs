0775ce1735a2cb234dd2612f43c36438d7437109
Otto <gonott@gmail.com>
2022-09-29 00:23:01 -0300

    Se crea objeto de conexion a base de datos.

---
+++ RSWork-Backend/DAO.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DAL
+{
+    internal class DAO
+    {
+
+        private SqlTransaction tx;
+        private static SqlConnection cn;
+
+        public static void Abrir()
+        {
+            cn = new SqlConnection("Initial Catalog=dbSegundoParcialCampo; Data Source=DESKTOP-VUD3NBA; Integrated Security=SSPI");
+            cn.Open();
+        }
+
+        public static void Cerrar()
+        {
+            cn.Close();
+            cn = null;
+            GC.Collect();
+        }
+
+        public void iniciarTransaccion()
+        {
+            tx = cn.BeginTransaction();
+        }
+
+
+        private static SqlCommand CrearComando(string Sql, List<IDbDataParameter> parametros = null, CommandType tipo = CommandType.StoredProcedure)
+        {
+            SqlCommand comando = new SqlCommand(Sql, cn);
+            comando.CommandType = tipo;
+            if (parametros != null && parametros.Count > 0)
+            {
+                comando.Parameters.AddRange(parametros.ToArray());
+            }
+            return comando;
+        }
+
+        public static int Escribir(string Sql, List<IDbDataParameter> parametros = null)
+        {
+            SqlCommand cmd = CrearComando(Sql, parametros);
+            int resultado;
+            try
+            {
+                resultado = cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+                //resultado = -1;
+            }
+
+            return resultado;
+        }
+
+
+        public static DataTable LeerConParametros(string Sql, List<IDbDataParameter> parametros)
+        {
+
+            SqlDataAdapter ad = new SqlDataAdapter();
+            ad.SelectCommand = CrearComando(Sql, parametros);
+            DataTable tabla = new DataTable();
+            ad.Fill(tabla);
+
+            return tabla;
+
+        }
+
+
+
+        public static DataTable Leer(string Sql, List<IDbDataParameter> parametros = null)
+        {
+            SqlDataAdapter ad = new SqlDataAdapter();
+            ad.SelectCommand = CrearComando(Sql, parametros);
+            DataTable tabla = new DataTable();
+            ad.Fill(tabla);
+
+            return tabla;
+        }
+
+        public static IDbDataParameter CrearParametro(string nom, string valor)
+        {
+            SqlParameter par = new SqlParameter(nom, valor);
+            par.DbType = DbType.String;
+            return par;
+        }
+
+        public static IDbDataParameter CrearParametro(string nom, decimal valor)
+        {
+            SqlParameter par = new SqlParameter(nom, valor);
+            par.DbType = DbType.Decimal;
+            return par;
+        }
+
+        public static IDbDataParameter CrearParametro(string nom, int valor)
+        {
+            SqlParameter par = new SqlParameter(nom, valor);
+            par.DbType = DbType.Int32;
+            return par;
+        }
+
+        public static IDbDataParameter CrearParametro(string nom, DateTime valor)
+        {
+            SqlParameter par = new SqlParameter(nom, valor);
+            par.DbType = DbType.DateTime;
+            return par;
+        }
+
+
+    }
+}

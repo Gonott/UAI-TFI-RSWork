4b98f8581d2f42dc4f7f34a4268bf1ae8e46c7da
Otto <gonott@gmail.com>
2022-09-29 00:23:25 -0300

    Se termina ABM de empleado en la parte logica.

--- RSWork-Backend/Empleado.cs
+++ RSWork-Backend/Empleado.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace BE
 {
@@ -18,7 +20,7 @@ namespace BE
 
         public string Nombre { get; set; }
 
-
+        
 
         public List<Elemento> Elementos { get; set; }
 
@@ -29,16 +31,76 @@ namespace BE
 
 namespace BLL
 {
-
+    using DAL;
+    using BE;
     public class EmpleadoBLL
     {
+
+        EmpleadoDAL mapper = new EmpleadoDAL();
+
 
         public List<BE.Empleado> ListarEmpleados(BE.Cliente cliente)
         {
             List<BE.Empleado> empleados = new List<BE.Empleado>();
+
+            try
+            {
+                empleados = mapper.listar(cliente);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
             return empleados;
         }
 
+
+        public void alta(Empleado empleado, Cliente empleador)
+        {
+
+            try
+            {
+                mapper.alta(empleado, empleador);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        public void baja(Empleado empleado, Cliente empleador)
+        {
+
+            try
+            {
+                mapper.baja(empleado, empleador);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        public void modificar(Empleado empleado)
+        {
+
+            try
+            {
+                mapper.modificar(empleado);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
 
 
         public bool ValidarElementos(List<BE.Empleado> empleados)
@@ -55,4 +117,90 @@ namespace BLL
 
 
     }
+}
+
+
+namespace DAL
+{
+    using BE;
+    using System.Data;
+    using System.Data.SqlClient;
+
+    public class EmpleadoDAL
+    {
+        public List<Empleado> listar(Cliente uncliente)
+        {
+           
+
+            List<Empleado> empleados = new List<Empleado>();
+
+            DAO.Abrir();
+            List<IDbDataParameter> parameters = new List<IDbDataParameter>();
+            parameters.Add(DAO.CrearParametro("@idcliente", uncliente.CodigoCliente));
+            DataTable tabla = DAO.LeerConParametros("ListarEmpleados",parameters);
+            DAO.Cerrar();
+
+            foreach (DataRow registro in tabla.Rows)
+            {
+                Empleado empleado = new Empleado();
+                empleado.DNI = registro.["DNI"];
+                empleado.Nombre = registro.["Nombre"];
+                empleado.Dirección = registro.["Direccion"];
+                empleado.Email = registro.["Email"];
+                           
+                empleados.Add(empleado);
+            }
+
+            return empleados;
+           
+
+
+        }
+
+
+        public void alta(Empleado empleado, Cliente empleador)
+        {
+            DAO.Abrir();
+            List<IDbDataParameter> parameters = new List<IDbDataParameter>();
+
+            parameters.Add(DAO.CrearParametro("@DNI",empleado.DNI));
+            parameters.Add(DAO.CrearParametro("@Nombre",empleado.Nombre));
+            parameters.Add(DAO.CrearParametro("@Direccion",empleado.Dirección));
+            parameters.Add(DAO.CrearParametro("@Email",empleado.Email));
+            parameters.Add(DAO.CrearParametro("@idempleador", empleador.CodigoCliente));
+            DAO.Escribir("AltaEmpleado", parameters);
+
+            DAO.Cerrar();
+        }
+
+
+        public void baja(Empleado empleado, Cliente empleador)
+        {
+            DAO.Abrir();
+            List<IDbDataParameter> parameters = new List<IDbDataParameter>();
+            parameters.Add(DAO.CrearParametro("@DNI", empleado.DNI));
+            parameters.Add(DAO.CrearParametro("@idempleador", empleador.CodigoCliente));
+            DAO.Escribir("BajaEmpleado", parameters);
+
+            DAO.Cerrar();
+        }
+
+
+        public void modificar(Empleado empleado)
+        {
+            DAO.Abrir();
+            List<IDbDataParameter> parameters = new List<IDbDataParameter>();
+
+            parameters.Add(DAO.CrearParametro("@DNI", empleado.DNI));
+            parameters.Add(DAO.CrearParametro("@Nombre", empleado.Nombre));
+            parameters.Add(DAO.CrearParametro("@Direccion", empleado.Dirección));
+            parameters.Add(DAO.CrearParametro("@Email", empleado.Email));
+            DAO.Escribir("ModificarEmpleado", parameters);
+
+            DAO.Cerrar();
+        }
+
+    }
+
+
 }

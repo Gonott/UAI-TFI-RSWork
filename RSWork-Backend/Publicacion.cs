9ca8a604310528fceb7968549ef37d5818b99c48
Otto <gonott@gmail.com>
2022-10-21 01:39:19 -0300

    Completado Back de Publicaciones en cuanto a bd y codigo

--- RSWork-Backend/Publicacion.cs
+++ RSWork-Backend/Publicacion.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BE;
+using DAL;
+using System.Data.Sql;
+using System.Data;
 
 namespace BE
 {
@@ -19,11 +23,9 @@ namespace BE
 
         public string Resumen { get; set; }
 
-        public BE.Elemento elemento{ get; set; }
+        public int codElemento{ get; set; }
 
-        public Proveedor proveedor { get; set; }
-
-
+        public int codProveedor { get; set; }
 
 
 
@@ -32,5 +34,194 @@ namespace BE
 
 
  }
+
+
+namespace BLL
+{
+
+    
+    public class PublicacionBLL
+    {PublicacionDAL mapper = new PublicacionDAL();
+
+        public void Alta(Publicacion publicacion)
+        {
+            try
+            {
+                mapper.Alta(publicacion);
+            }
+            catch (Exception exx)
+            {
+
+                throw exx;
+            }
+        }
+
+
+        public void Baja(Publicacion publicacion)
+        {
+            try
+            {
+                mapper.baja(publicacion);
+            }
+            catch (Exception exx)
+            {
+
+                throw exx;
+            }
+        }
+
+
+
+        public void Modificar(Publicacion publicacion)
+        {
+            try
+            {
+                mapper.Modificar(publicacion);
+            }
+            catch (Exception exx)
+            {
+
+                throw exx;
+            }
+        }
+        
+
+
+        public List<Publicacion> ListarPorProveedor(Proveedor proveedor)
+        {
+            try
+            {
+                return mapper.listarMisPub(proveedor);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+
+
+        public List<Publicacion> ListarTodas()
+        {
+            try
+            {
+                return mapper.ListarTodo();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+    }
+    
+}
+
+namespace DAL
+{
+    public class PublicacionDAL
+    {
+        public void Alta(Publicacion publicacion)
+        {
+            DAO.Abrir();
+            List<IDbDataParameter> parameters = new List<IDbDataParameter>();
+
+            parameters.Add(DAO.CrearParametro("@id", publicacion.Id));
+            parameters.Add(DAO.CrearParametro("@cantidad", publicacion.Cantidad));
+            parameters.Add(DAO.CrearParametro("@nombre", publicacion.Nombre));
+            parameters.Add(DAO.CrearParametro("@imagen", publicacion.Imagen));
+            parameters.Add(DAO.CrearParametro("@resumen", publicacion.Resumen));
+            parameters.Add(DAO.CrearParametro("@codproveedor", publicacion.codProveedor));
+            parameters.Add(DAO.CrearParametro("@codelemento", publicacion.codElemento));
+            DAO.Escribir("AltaPublicacion", parameters);
+
+            DAO.Cerrar();
+        }
+
+        public void baja(Publicacion publicacion)
+        {
+            DAO.Abrir();
+            List<IDbDataParameter> parameters = new List<IDbDataParameter>();
+            parameters.Add(DAO.CrearParametro("@id", publicacion.Id));
+            DAO.Escribir("BajaPublicacion", parameters);
+
+            DAO.Cerrar();
+        }
+
+
+        public void Modificar(Publicacion publicacion)
+        {
+            DAO.Abrir();
+            List<IDbDataParameter> parameters = new List<IDbDataParameter>();
+
+            parameters.Add(DAO.CrearParametro("@id", publicacion.Id));
+            parameters.Add(DAO.CrearParametro("@cantidad", publicacion.Cantidad));
+            parameters.Add(DAO.CrearParametro("@nombre", publicacion.Nombre));
+            parameters.Add(DAO.CrearParametro("@imagen", publicacion.Imagen));
+            parameters.Add(DAO.CrearParametro("@resumen", publicacion.Resumen));
+            parameters.Add(DAO.CrearParametro("@codproveedor", publicacion.codProveedor));
+            parameters.Add(DAO.CrearParametro("@codelemento", publicacion.codElemento));
+            DAO.Escribir("ModificarPublicacion", parameters);
+
+            DAO.Cerrar();
+        }
+
+        public List<Publicacion> listarMisPub(Proveedor proveedor)
+        {
+            List<Publicacion> publicaciones = new List<Publicacion>();
+            DAO.Abrir();
+            List<IDbDataParameter> parameters = new List<IDbDataParameter>();
+            parameters.Add(DAO.CrearParametro("@codigoproveedor", proveedor.CodigoProveedor));
+            DataTable tabla = DAO.LeerConParametros("ListarPublicacionesProveedor", parameters); 
+            DAO.Cerrar();
+
+            foreach (DataRow registro in tabla.Rows)
+            {
+                Publicacion publicacion = new Publicacion();
+                publicacion.Id = int.Parse(registro["Id"].ToString());
+                publicacion.Cantidad = int.Parse(registro["Cantidad"].ToString());
+                publicacion.Nombre = registro["Nombre"].ToString();
+                publicacion.Imagen = registro["Imagen"].ToString();
+                publicacion.Resumen = registro["Resumen"].ToString();
+                publicacion.codElemento = int.Parse(registro["codElemento"].ToString());
+                publicacion.codProveedor = int.Parse(registro["codProveedor"].ToString());
+                publicaciones.Add(publicacion);
+            }
+            return publicaciones;
+
+        }
+
+
+        public List<Publicacion> ListarTodo()
+        {
+            List<Publicacion> publicaciones = new List<Publicacion>();
+            DAO.Abrir();
+            List<IDbDataParameter> parameters = new List<IDbDataParameter>();
+            DataTable tabla = DAO.Leer("ListarPublicaciones", parameters); 
+
+            DAO.Cerrar();
+
+            foreach (DataRow registro in tabla.Rows)
+            {
+                Publicacion publicacion = new Publicacion();
+                publicacion.Id = int.Parse(registro["Id"].ToString());
+                publicacion.Cantidad = int.Parse(registro["Cantidad"].ToString());
+                publicacion.Nombre = registro["Nombre"].ToString();
+                publicacion.Imagen = registro["Imagen"].ToString();
+                publicacion.Resumen = registro["Resumen"].ToString();
+                publicacion.codElemento = int.Parse(registro["codElemento"].ToString());
+                publicacion.codProveedor = int.Parse(registro["codProveedor"].ToString());
+                publicaciones.Add(publicacion);
+
+            }
+            return publicaciones;
+        }
+
+    }
+
+
+}
 
 

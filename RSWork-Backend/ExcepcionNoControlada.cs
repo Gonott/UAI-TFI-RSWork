4cc3ccb9eb0fb6f2fc353bbd199415bb0e33e16e
Otto <gonott@gmail.com>
2022-11-27 13:00:22 -0300

    Creacion de objeto para manejo de excepciones a otra pagina.

---
+++ RSWork-Backend/ExcepcionNoControlada.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SERVICIOS
+{
+    class ExcepcionNoControlada
+    {
+
+        public string Tipo { get; set; }
+
+        public DateTime TimeStamp { get; set; }
+        public string Message { get; set; }
+        public string StackTrace { get; set; }
+
+
+
+    }
+}
